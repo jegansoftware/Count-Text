279fde7b6c756fc51fa9dd35848fd9072d5e07bd
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-27 03:27:46 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package EmployeeList;
+
+import java.util.*;
+
+public class jegan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Text n=new Text();
+		n.textanalyze();
+		n.Display();
+		
+			}
+
+}
